d351e0e6a5d932ed8731cc5a2901001d428eb50b
Juan <juan.aedo.contreras@gmail.com>
2024-06-03 21:07:17 -0400

    Esta es la última versión del proyecto

--- app/src/main/java/com/example/safenav/MenuActivity.kt
+++ app/src/main/java/com/example/safenav/MenuActivity.kt
@@ -1,6 +1,8 @@
 package com.example.safenav
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,13 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val tvGoMenu = findViewById<TextView>(R.id.tv_go_to_menu)
+        tvGoMenu.setOnClickListener{
+            goToMenu()
         }
+    }
+    private fun goToMenu(){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
     }
 }
